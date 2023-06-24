4f807ffcf70ef98dc106d4ee112321ca041098e6
kostandinvllahu <vllahukostandin@gmail.com>
2023-06-24 10:20:37 +0200

    Removed a comment

--- app/Http/Requests/SettingsRequest.php
+++ app/Http/Requests/SettingsRequest.php
@@ -58,46 +58,5 @@ class SettingsRequest extends FormRequest
      * @param  \Illuminate\Validation\Validator  $validator
      * @return void
      */
-    // public function withValidator(Validator $validator): void
-    // {
-    //     $validator->after(function (Validator $validator) {
-    //         // Only perform additional validation if the checkbox is checked
-    //         if ($this->input('change_password')) {
-    //             $userId = Auth::user()->id;
-    //             $data = $this->all();
 
-    //             $password = $data['password'] ?? null;
-    //             $newPassword = $data['new_password'] ?? null;
-    //             $confirmPassword = $data['confirm_password'] ?? null;
-
-    //             // Check if the password is correct for the current user
-    //             if ($password && !Auth::attempt(['id' => $userId, 'password' => $password])) {
-    //                 $validator->errors()->add('password', 'The current password is incorrect.');
-    //             }
-
-    //             // Check if the new password matches the confirmation
-    //             if ($newPassword !== $confirmPassword) {
-    //                 $validator->errors()->add('confirm_password', 'The new password confirmation does not match.');
-    //             }
-
-    //             // Check if the name and email exist for another user
-    //             $existingUser = User::where('id', '!=', $userId)
-    //                 ->where(function ($query) use ($data) {
-    //                     $query->where('name', $data['name'])
-    //                         ->orWhere('email', $data['email']);
-    //                 })
-    //                 ->first();
-
-    //             if ($existingUser) {
-    //                 if ($existingUser->name === $data['name']) {
-    //                     $validator->errors()->add('name', 'The name is already taken by another user.');
-    //                 }
-
-    //                 if ($existingUser->email === $data['email']) {
-    //                     $validator->errors()->add('email', 'The email is already taken by another user.');
-    //                 }
-    //             }
-    //         }
-    //     });
-    // }
 }
