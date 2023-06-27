f1abe94a2afb5a44bdc9f5a652a0cb87d4f14968
kostandinvllahu <vllahukostandin@gmail.com>
2023-06-27 21:55:28 +0200

    Made the search on friend request panel that you can search only your friends

--- app/Http/Controllers/FriendRequestController.php
+++ app/Http/Controllers/FriendRequestController.php
@@ -16,19 +16,37 @@ class FriendRequestController extends Controller
     }
 
     public function index(Request $request)
-    {
-        $user = Auth::user();
+{
+    $user = Auth::user();
+    
+    $searchTerm = $request->input('search', '');
+    $users = [];
+
+    if ($searchTerm) {
+        $userIds = User::where('name', 'like', '%' . $searchTerm . '%')
+            ->where('id', '!=', $user->id)
+            ->pluck('id');
+
+        $userFriends = Friend::whereIn('userId', $userIds)
+            ->where('friendsId', $user->id)
+            ->get();
+
+        $users = User::whereIn('id', $userFriends->pluck('userId'))->get();
+    } else {
         $userFriends = Friend::where('friendsId', $user->id)->get();
         $users = User::whereIn('id', $userFriends->pluck('userId'))->get();
-
-        return view('friendRequest.index', [
-            'users' => $users,
-            'userFriends' => $userFriends,
-        ]);
     }
 
+    return view('friendRequest.index', [
+        'users' => $users,
+        'userFriends' => $userFriends,
+        'searchTerm' => $searchTerm,
+    ]);
+}
+
+
     public function update(Request $request, $friendId)
-{
+    {
     $user = Auth::user();
 
     $query = Friend::where('userId', $friendId)
@@ -47,7 +65,6 @@ class FriendRequestController extends Controller
     ]);
 
     return redirect()->back()->with('success', 'Friends accepted successfully.');
-}
-
-
+    }   
+    
 }
--- resources/views/friendRequest/index.blade.php
+++ resources/views/friendRequest/index.blade.php
@@ -4,8 +4,8 @@
     <div class="container">
         <h1>Search and Table Example</h1>
 
-        <form>
-            <input type="text" name="search" class="form-control" placeholder="Search..." >
+        <form method="GET" action="{{ route('friendRequest.index') }}">
+            <input type="text" name="search" class="form-control" placeholder="Search..." value="{{ $searchTerm }}">
             <br>
             <button type="submit" class="btn btn-primary">Search</button>
         </form>
@@ -58,5 +58,33 @@
                 @endforeach
                 </tbody>
             </table>
+            <input type="hidden" name="searchTerm" value="{{ $searchTerm }}">
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <script>
+        // Add event listener to the search input
+        var searchInput = document.querySelector('input[name="search"]');
+
+        searchInput.addEventListener('input', function() {
+            var searchTerm = searchInput.value.toLowerCase();
+            var table = document.querySelector('table');
+            var rows = table.getElementsByTagName('tr');
+
+            for (var i = 0; i < rows.length; i++) {
+                var name = rows[i].getElementsByTagName('td')[1];
+
+                if (name) {
+                    var textValue = name.textContent || name.innerText;
+                    if (textValue.toLowerCase().indexOf(searchTerm) > -1) {
+                        rows[i].style.display = '';
+                    } else {
+                        rows[i].style.display = 'none';
+                    }
+                }
+            }
+        });
 @endsection
